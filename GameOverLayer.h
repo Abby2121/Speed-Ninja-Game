09662c347383afc8080b0323180bb55aeb107c3b
Abigail Thurmond <AbigailThurmond14@gmail.com>
2013-06-05 19:48:39 -0600

    GameOverLayer.h

--- GameOverLayer.h
+++ GameOverLayer.h
@@ -1,66 +1,18 @@
 //
-//  IntroLayer.m
+//  GameOverLayer.h
 //  Speed Ninja
 //
-//  Created by Abigail Thurmond on 5/17/13.
-//  Copyright Abigail Thurmond 2013. All rights reserved.
+//  Created by Abigail Thurmond on 5/19/13.
+//  Copyright 2013 Abigail Thurmond. All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
 
-// Import the interfaces
-#import "IntroLayer.h"
-#import "HelloWorldLayer.h"
+@interface GameOverLayer : CCLayerColor
+    
++(CCScene *) sceneWithWon:(BOOL)won;
+-(id)initWithWon:(BOOL)won;
 
 
-#pragma mark - IntroLayer
-
-// HelloWorldLayer implementation
-@implementation IntroLayer
-
-// Helper class method that creates a Scene with the HelloWorldLayer as the only child.
-+(CCScene *) scene
-{
-  // 'scene' is an autorelease object.
-	CCScene *scene = [CCScene node];
-	
-	// 'layer' is an autorelease object.
-	IntroLayer *layer = [IntroLayer node];
-	
-	// add layer as a child to scene
-	[scene addChild: layer];
-	
-	// return the scene
-	return scene;
-}
-
-// 
--(id) init
-{
-	if( (self=[super init])) {
-
-		// ask director for the window size
-		CGSize size = [[CCDirector sharedDirector] winSize];
-
-		CCSprite *background;
-		
-		if( UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone ) {
-			background = [CCSprite spriteWithFile:@"Default.png"];
-			background.rotation = 90;
-		} else {
-			background = [CCSprite spriteWithFile:@"Default-Landscape~ipad.png"];
-		}
-		background.position = ccp(size.width/2, size.height/2);
-
-		// add the label as a child to this Layer
-		[self addChild: background];
-	}
-	
-	return self;
-}
-
--(void) onEnter
-{
-	[super onEnter];
-	[[CCDirector sharedDirector] replaceScene:[CCTransitionFade transitionWithDuration:1.0 scene:[HelloWorldLayer scene] ]];
-}
 @end
